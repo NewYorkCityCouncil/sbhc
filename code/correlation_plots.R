50a247fcdf8c4a3f846b7902777840a77a0c162d
Reese Hirota <122996073+rhirotacouncil@users.noreply.github.com>
2024-04-11 10:25:56 -0400

    correlation plot

--- code/correlation_plots.R
+++ code/correlation_plots.R
@@ -13,7 +13,7 @@ library(sf)
 library(janitor)
 
 # ---- load data ----
-health_data <- read.csv("data/input/health_master_2122.csv") %>% clean_names() # copy pasted 2021-2022 LL12, DOE, data by school dist
+health_data <- read.csv("../data/input/health_master_2122.csv") %>% clean_names() # copy pasted 2021-2022 LL12, DOE, data by school dist
 # append vaccination info from OpenData
 vax <- vroom::vroom("https://data.cityofnewyork.us/resource/ne9b-qgmm.csv?$limit=999999999")
 # ---- clean data ----
@@ -36,6 +36,11 @@ health_data$n_not_fully_vaccinated = health_data$total_enrollment - health_data$
 p1 <- ggplot(data=health_data, aes(x=diabetes_type_1/total_enrollment, 
                                    y = total_sbh_cs)) +
   geom_point(size = health_data$total_enrollment/20000) +
+  geom_label(data = health_data[health_data$geographic_community_school_district %in% c(9,10),],
+             aes(x = diabetes_type_1/total_enrollment,total_sbh_cs,
+                 label=geographic_community_school_district),
+             nudge_y = .5, nudge_x = -.00015,
+             size = 1.8) +
   labs(x = "Diabetes 1 \n(% Total Enrollment)", y = NULL) +
   scale_x_continuous(labels = scales::percent) +
   theme_nycc()
@@ -45,6 +50,11 @@ p1 <- ggplot(data=health_data, aes(x=diabetes_type_1/total_enrollment,
 p2 <- ggplot(data=health_data, aes(x=diabetes_type_2/total_enrollment, 
                                    y = total_sbh_cs)) + # static vers
   geom_point(size = health_data$total_enrollment/20000) +
+  geom_label(data = health_data[health_data$geographic_community_school_district %in% c(9,10),],
+             aes(x = diabetes_type_2/total_enrollment,total_sbh_cs,
+                 label=geographic_community_school_district),
+             nudge_y = .5, nudge_x = .000075,
+             size = 1.8) +
   labs(x = "Diabetes 2 \n(% Total Enrollment)", y = NULL) +
   scale_x_continuous(labels = scales::percent) +
   theme_nycc()
@@ -54,6 +64,11 @@ p2 <- ggplot(data=health_data, aes(x=diabetes_type_2/total_enrollment,
 p3 <- ggplot(data=health_data, aes(x=asthma/total_enrollment, 
                                    y = total_sbh_cs)) + # static vers
   geom_point(size = health_data$total_enrollment/20000) +
+  geom_label(data = health_data[health_data$geographic_community_school_district %in% c(9,10),],
+             aes(x = asthma/total_enrollment,total_sbh_cs,
+                 label=geographic_community_school_district),
+             nudge_y = 1.5, nudge_x = .01,
+             size = 1.8) +
   labs(x = "Asthma \n(% Total Enrollment)", y = NULL) +
   scale_x_continuous(labels = scales::percent) +
   theme_nycc()
@@ -63,6 +78,11 @@ p3 <- ggplot(data=health_data, aes(x=asthma/total_enrollment,
 p4 <- ggplot(data=health_data, aes(x=anaphylaxis_allergies/total_enrollment, 
                                    y = total_sbh_cs)) + # static vers
   geom_point(size = health_data$total_enrollment/20000) +
+  geom_label(data = health_data[health_data$geographic_community_school_district %in% c(9,10),],
+             aes(x = anaphylaxis_allergies/total_enrollment,total_sbh_cs,
+                 label=geographic_community_school_district),
+             nudge_y = 1.5, nudge_x = -.002,
+             size = 1.8) +
   labs(x = "Anaphylaxis \n(% Total Enrollment)", y = NULL) +
   scale_x_continuous(labels = scales::percent) +
   theme_nycc()
@@ -72,6 +92,11 @@ p4 <- ggplot(data=health_data, aes(x=anaphylaxis_allergies/total_enrollment,
 p5 <- ggplot(data=health_data, aes(x=x_poverty/total_enrollment, 
                                    y = total_sbh_cs)) + # static vers
   geom_point(size = health_data$total_enrollment/20000) +
+  geom_label(data = health_data[health_data$geographic_community_school_district %in% c(9,10),],
+             aes(x = x_poverty/total_enrollment,total_sbh_cs,
+                 label=geographic_community_school_district),
+             nudge_y = 1.55, nudge_x = -.002,
+             size = 1.8) +
   labs(x = "Poverty \n(% Total Enrollment)", y = NULL) +
   scale_x_continuous(labels = scales::percent) +
   theme_nycc()
@@ -79,44 +104,67 @@ p5 <- ggplot(data=health_data, aes(x=x_poverty/total_enrollment,
 
 # Vaccinations (6, OpenData)
 
-p6 <- ggplot(data=health_data, aes(x=n_not_fully_vaccinated/total_enrollment, 
-                                   y = total_sbh_cs)) + # static vers
-  geom_point(size = health_data$total_enrollment/20000) +
-  labs(x = "Not Fully Vaccinated \n(% Total Enrollment)", y = NULL) +
-  scale_x_continuous(labels = scales::percent) +
-  theme_nycc()
+# p6 <- ggplot(data=health_data, aes(x=n_not_fully_vaccinated/total_enrollment, 
+#                                    y = total_sbh_cs)) + # static vers
+#   geom_point(size = health_data$total_enrollment/20000) +
+#   geom_label(data = health_data[health_data$geographic_community_school_district %in% c(9,10),],
+#              aes(x = x_poverty/total_enrollment,total_sbh_cs,
+#                  label=geographic_community_school_district),
+#              nudge_y = 1.55, nudge_x = -.002,
+#              size = 1.8) +
+#   labs(x = "Not Fully Vaccinated \n(% Total Enrollment)", y = NULL) +
+#   scale_x_continuous(labels = scales::percent) +
+#   theme_nycc()
 
 
-# Medication orders (7, LL12)
-
-p7 <- ggplot(data=health_data, aes(x=total_number_of_medication_orders_reviewed_by_osh, 
-                                   y = total_sbh_cs)) + # static vers
-  geom_point(size = health_data$total_enrollment/20000) +
-  labs(x = "Medication Orders", y = NULL) + 
-  geom_label(data = health_data[health_data$total_number_of_medication_orders_reviewed_by_osh > 6000,],
-             size=(health_data[health_data$total_number_of_medication_orders_reviewed_by_osh > 6000,]$total_enrollment/20000),
-             aes(x = total_number_of_medication_orders_reviewed_by_osh,total_sbh_cs,
-                 label=geographic_community_school_district),
-             nudge_y = 3, nudge_x = -200) +
-  scale_x_continuous(labels = scales::comma) +
-  theme_nycc() 
-
-# Student Encounters (8, LL12)
-
-p8 <- ggplot(data=health_data, aes(x=total_number_of_student_encounters_of_all_med_room_visits_in_ashr, 
-                                   y = total_sbh_cs)) + # static vers
-  geom_point(size = health_data$total_enrollment/20000) +
-  geom_label(data = health_data[health_data$total_number_of_student_encounters_of_all_med_room_visits_in_ashr > 150000,],
-                   size=(health_data[health_data$total_number_of_student_encounters_of_all_med_room_visits_in_ashr > 150000,]$total_enrollment/20000),
-                   aes(x = total_number_of_student_encounters_of_all_med_room_visits_in_ashr,total_sbh_cs,
-                       label=geographic_community_school_district),
-             nudge_y = 3, nudge_x = -5000) +
-  labs(x = "Student Encounters", y = NULL) + 
-  scale_x_continuous(labels = scales::comma) +
-  theme_nycc()
+# # Medication orders (7, LL12)
+# 
+# p7 <- ggplot(data=health_data, aes(x=total_number_of_medication_orders_reviewed_by_osh, 
+#                                    y = total_sbh_cs)) + # static vers
+#   geom_point(size = health_data$total_enrollment/20000) +
+#   labs(x = "Medication Orders", y = NULL) + 
+#   geom_label(data = health_data[health_data$total_number_of_medication_orders_reviewed_by_osh > 6000,],
+#              size=(health_data[health_data$total_number_of_medication_orders_reviewed_by_osh > 6000,]$total_enrollment/20000),
+#              aes(x = total_number_of_medication_orders_reviewed_by_osh,total_sbh_cs,
+#                  label=geographic_community_school_district),
+#              nudge_y = 3, nudge_x = -200) +
+#   scale_x_continuous(labels = scales::comma) +
+#   theme_nycc() 
+# 
+# # Student Encounters (8, LL12)
+# 
+# p8 <- ggplot(data=health_data, aes(x=total_number_of_student_encounters_of_all_med_room_visits_in_ashr, 
+#                                    y = total_sbh_cs)) + # static vers
+#   geom_point(size = health_data$total_enrollment/20000) +
+#   geom_label(data = health_data[health_data$total_number_of_student_encounters_of_all_med_room_visits_in_ashr > 150000,],
+#                    size=(health_data[health_data$total_number_of_student_encounters_of_all_med_room_visits_in_ashr > 150000,]$total_enrollment/20000),
+#                    aes(x = total_number_of_student_encounters_of_all_med_room_visits_in_ashr,total_sbh_cs,
+#                        label=geographic_community_school_district),
+#              nudge_y = 3, nudge_x = -5000) +
+#   labs(x = "Student Encounters", y = NULL) + 
+#   scale_x_continuous(labels = scales::comma) +
+#   theme_nycc()
 
 # grid all
-grid.arrange(p1,p2,p3, p4, p5, p6, p7, p8,
+grid.arrange(main, p5,p1,p2, p4, p3, # p6, # p7, p8, - commenting out, data includes nurses offices, hiding vaccinations too
              ncol = 2,
              left = "Number of School Based Health Clinics in District")
+
+# enrollment/sbhc outliers
+model <- lm(total_sbh_cs ~ total_enrollment, data = health_data)
+stud_resids <- studres(model)
+plot(health_data$geographic_community_school_district, stud_resids,  
+     ylab='Studentized Residuals', xlab='School District') # school dists 9, 10
+
+main <- ggplot(data=health_data, aes(x=total_enrollment, 
+                             y = total_sbh_cs)) +
+  geom_point(size = health_data$total_enrollment/20000) +
+  # geom_smooth(method='lm', se = FALSE) +
+  geom_label(data = health_data[health_data$geographic_community_school_district %in% c(9,10),],
+             aes(x = total_enrollment,total_sbh_cs,
+                 label=geographic_community_school_district),
+             nudge_y = -2, nudge_x = -1500,
+             size = 1.4)  +
+  labs(x = "Total Enrollment", y = NULL) +
+  theme_nycc()
 
