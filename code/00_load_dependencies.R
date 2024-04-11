9ce161f1af8bf2fe22807e82c8d0af9ab70a5f51
rose <romartinez@council.nyc.gov>
2024-04-11 15:04:35 -0400

    update pkgs in load dependencies

--- code/00_load_dependencies.R
+++ code/00_load_dependencies.R
@@ -5,7 +5,8 @@
 #' 
 #' IF YOU DO NOT WANT TO INSTALL ANY OF THESE PACKAGES, DO NOT RUN THIS CODE.
 
-list.of.packages <- c("tidyverse", "janitor","sf","readxl","jsonlite","stringr","ggiraph","mapview","RSocrata","geojsonsf","sf","leaflet","leaflet.extras","htmltools","scales","classInt")
+list.of.packages <- c("tidyverse", "janitor","sf","readxl","jsonlite","stringr","ggiraph","mapview","RSocrata","geojsonsf","sf","leaflet","leaflet.extras","htmltools","scales","classInt", "readxl", "tigris",
+                      "patchwork")
 
 # checks if packages has been previously installed
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
