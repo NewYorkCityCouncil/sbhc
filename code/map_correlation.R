1b9ff4d961a12d2c730e612824090a77856bae4e
Reese Hirota <122996073+rhirotacouncil@users.noreply.github.com>
2024-04-15 15:10:56 -0400

    formatting

--- code/map_correlation.R
+++ code/map_correlation.R
@@ -1,4 +1,4 @@
-
+library(ggpubr)
 # run 01_prepare-data.R first - need school_dist_shp, map_sf
 source("code/01_prepare-data.R")
 
@@ -25,7 +25,10 @@ p_map <- ggplot(NULL) +
                       aes(fill = percent_poverty,
                           data_id = school_dis)) +
   geom_sf_label(data=school_dist_shp, aes(label = school_dis), 
-                label.size  = NA, alpha = 0) +
+                label.size  = .05, 
+                alpha = .2, 
+                size =2,
+                color = "navy") +
   # geom_point(data = map_sf, # add schools
   #            aes(x = longitude, y = latitude,
   #                col="grey", alpha = 0.2)) + 
@@ -44,9 +47,12 @@ p_map <- ggplot(NULL) +
 p_plot <- ggplot(school_dist_shp, 
        aes(x = percent_poverty, y = num_sbhcs, color = percent_poverty,
            data_id = school_dis)) + 
+  stat_cor(method="pearson") +
   geom_point_interactive(size = (school_dist_shp$total_enrollment)/10000,
                          show.legend = FALSE,
-                         aes(tooltip = paste0("SBHCs: ", num_sbhcs))) + 
+                         aes(tooltip = paste0("<strong>School District: </strong>", school_dis, "<br>",
+                                              "SBHCs: ", num_sbhcs, "<br>", 
+                                              "Poverty: ", round(percent_poverty*100,0), "%"))) + 
   scale_color_distiller(direction = 1) +
   # scale_y_log10() + 
   scale_x_continuous(labels = scales::percent) +
@@ -60,7 +66,13 @@ map_plot <- girafe(ggobj = p_plot + p_map, width_svg = 10, height_svg = 5.5)  %>
   girafe_options(opts_zoom(min = 1, max = 8),
                  opts_selection(
                    css = "fill:moccasin;",
-                   only_shiny = FALSE)
+                   only_shiny = FALSE),
+                 opts_tooltip(
+                   opacity = 0.8,
+                   css = "background-color:#4c6061; color:white; padding:10px; border-radius:5px;"),
+                 #opts_hover_inv(css = "stroke-width: 1;opacity:0.6;"),
+                 #opts_hover(css = "stroke-width: 4; opacity: 1; color:orange"),
+                 opts_toolbar(pngname = "poverty_correlation")
   )
 
 htmltools::save_html(map_plot, "visuals/poverty-correlation-map_interactive.html")
